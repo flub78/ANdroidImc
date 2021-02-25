b6e7e8851158f63864647e7d0aa37f8794a7a8c8
flub78 <frederic.peignot@free.fr>
2021-02-25 11:05:24 +0100

    Small refactoring

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ OpenClassroom "Créez des applicationspour Android"
 Section "Les widgets lesplus simples"
 
 This app compute your body mass index (BMI) or IMC in French.
+
+## Features
+
+- Compute MBI
+
+## To do list
+
+### DevOps
+- Implement unit tests
+
+### Refactoring
+
+- Change toast attributes
+- Include drawable images, icons in the toast messages, etc.
+
+### Features
+- Change BMI color depending on the range
+- Store history
+- Display curves
+- Manage local en, fr
+- Manage several profiles
+
--- app/src/main/java/flub78/org/imc/MainActivity.java
+++ app/src/main/java/flub78/org/imc/MainActivity.java
@@ -3,13 +3,19 @@ package flub78.org.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
+
+import static android.widget.Toast.*;
+import static flub78.org.imc.R.id.custom_toast_container;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         mResult = (TextView) findViewById(R.id.activity_main_result_text);
         mSizeInput = (EditText) findViewById(R.id.activity_main_size_input);
@@ -45,24 +51,24 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     weight = Float.parseFloat(mWeightInput.getText().toString());
                 } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Poid incorrect", Toast.LENGTH_SHORT).show();
+                    warningPopup("Poids incorrect");
                     return;
                 }
 
                 try {
                     size = Float.parseFloat(mSizeInput.getText().toString());
                 } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Taille incorrecte", Toast.LENGTH_SHORT).show();
+                    warningPopup("Taille incorrecte");
                     return;
                 }
 
                 if ((size < 1.0) || (size > 2.2)) {
-                    Toast.makeText(getApplicationContext(), "IMC non significatif pour cette taille", Toast.LENGTH_SHORT).show();
+                    warningPopup("IMC non significatif pour cette taille");
                     return;
                 }
 
                 if ((weight < 40.0) || (weight > 200.2)) {
-                    Toast.makeText(getApplicationContext(), "IMC non significatif pour ce poid", Toast.LENGTH_SHORT).show();
+                    warningPopup("IMC non significatif pour ce poids");
                     return;
                 }
 
@@ -83,6 +89,30 @@ public class MainActivity extends AppCompatActivity {
                 mResult.setText("");
             }
         });
+
+    }
+
+
+    /**
+     * Small warning for input errors
+     * @param msg
+     */
+    void warningPopup(final String msg) {
+
+        // Toast.makeText(getApplicationContext(), "Poid incorrect", Toast.LENGTH_SHORT).show();
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(custom_toast_container));
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText(msg);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
 
     }
 }
